66cada23e6957d78789c649c87abb01590265eec
Haque Mohammad Reduwan <49554047+Ridzzz0Alam@users.noreply.github.com>
2023-11-26 12:57:34 +0800

    Add files via upload
    Web Development project practice using HTML, CSS and JavaScript

---
+++ Digital Clock/script.js
@@ -0,0 +1,28 @@
+//update Date
+function updateDate(){
+    const now = new Date();
+    const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    const dateString = now.toLocaleDateString(undefined,options);
+    document.querySelector('.date').textContent = dateString; 
+}
+
+
+// Update Time
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const timeString = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
+    document.querySelector('.time').textContent = timeString;
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+
+// callback functions 
+updateDate();
+setInterval(updateTime, 1000);
